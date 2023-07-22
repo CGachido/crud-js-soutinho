79277a8eec8cc4254489a9b406de581dcb360f7d
Cassius Souza <cassius.sanches@hotmail.com>
2023-07-22 17:01:31 -0300

    change status todo backend

--- pages/api/todos/[id]/toggle-done.ts
+++ pages/api/todos/[id]/toggle-done.ts
@@ -5,15 +5,9 @@ export default function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  if (request.method === "GET") {
-    todoController.get(request, response);
+  if (request.method === "PUT") {
+    todoController.toggleDone(request, response);
     return;
   }
-
-  if (request.method === "POST") {
-    todoController.create(request, response);
-    return;
-  }
-
   response.status(405).json({ error: { message: "Method not allowed" } });
 }
